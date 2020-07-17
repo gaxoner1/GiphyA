71bdf559d0f912eabfc4ab3fdf3d04f62db94c13
Geoffrey Andrade <30633908+gaxoner1@users.noreply.github.com>
2020-07-16 21:28:42 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import App2 from "./App2";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App2 />
+  </React.StrictMode>,
+  rootElement
+);
